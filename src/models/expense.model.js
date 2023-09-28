c64e1311721ae1bc99a8092b54a42a9fc433f906
stiven <strujillodev@gmail.com>
2023-09-27 23:14:57 -0500

    feat: add expense model

---
+++ src/models/expense.model.js
@@ -0,0 +1,49 @@
+import { Schema, model } from "mongoose";
+
+const expenseSchema = new Schema(
+  {
+    note: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    month: {
+      type: Number,
+      required: true,
+    },
+    day: {
+      type: Number,
+      required: true,
+    },
+    monthName: {
+      type: String,
+      required: true,
+    },
+    dayName: {
+      type: String,
+      required: true,
+    },
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+expenseSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export const Expense = model(
+  "Expense",
+  expenseSchema
+); /* El nombre debe ser siempre en singular */
