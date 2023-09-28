925ebbe11139fe5bed2d19c5fb4e9d26b7289df2
stiven <strujillodev@gmail.com>
2023-09-27 23:15:04 -0500

    add revenue model

---
+++ src/models/revenue.model.js
@@ -0,0 +1,49 @@
+import { Schema, model } from "mongoose";
+
+const revenueSchema = new Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    month: {
+      type: Number,
+      required: true,
+    },
+    day: {
+      type: Number,
+      required: true,
+    },
+    monthName: {
+      type: String,
+      required: true,
+    },
+    dayName: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+revenueSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export const Revenue = model(
+  "Revenue",
+  revenueSchema
+); /* El nombre debe ser siempre en singular */
