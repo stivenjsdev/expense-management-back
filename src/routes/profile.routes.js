1cd4e235dd1cfe13de312c128e5c0b247d84b340
stiven <strujillodev@gmail.com>
2023-09-30 10:13:40 -0500

    feat: add routes from profile controller

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { getProfile } from "../controllers/profile.controller.js";
+import {
+  createExpense,
+  createProduct,
+  createRevenue,
+  getCurrentValues,
+  getProfile,
+} from "../controllers/profile.controller.js";
 import { checkToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 // router.method('path', middleware, controller);
 router.get("/me", checkToken, getProfile);
+router.get("/currentTotals", checkToken, getCurrentValues);
+router.post("/product", checkToken, createProduct);
+router.post("/expense", checkToken, createExpense);
+router.post("/revenue", checkToken, createRevenue);
 
 export default router;
